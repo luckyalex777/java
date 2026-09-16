afd32e32343335f276b2249f5d86a3f70a38a10b
Alex Savelyev <luckyalex777@gmail.com>
2026-09-16 19:11:28 -0400

    Project for bencode library created

---
+++ universe/bencode-library/src/main/java/com/alexswd/bencode/BencodeDecoder.java
@@ -0,0 +1,22 @@
+package com.alexswd.bencode;
+
+/**
+ * Decoder for bencode format.
+ *
+ * Bencode is a simple encoding format used primarily in torrent files. This class provides methods
+ * to decode bencode-encoded data.
+ */
+public class BencodeDecoder {
+
+  /**
+   * Decodes a bencode-encoded string.
+   *
+   * @param encoded the bencode-encoded string to decode
+   * @return the decoded object
+   * @throws IllegalArgumentException if the bencode format is invalid
+   */
+  public Object decode(String encoded) {
+    // TODO: Implement bencode decoding
+    throw new UnsupportedOperationException("Bencode decoding not yet implemented");
+  }
+}
---
+++ universe/bencode-library/src/test/java/com/alexswd/bencode/BencodeDecoderTest.java
@@ -0,0 +1,50 @@
+package com.alexswd.bencode;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for BencodeDecoder.
+ */
+public class BencodeDecoderTest {
+
+  private final BencodeDecoder decoder = new BencodeDecoder();
+
+  /**
+   * Test decoding of integer bencode format.
+   */
+  @Test
+  public void testDecodeInteger() {
+    // TODO: Implement test for integer decoding
+    assertThat(true, equalTo(true));
+  }
+
+  /**
+   * Test decoding of string bencode format.
+   */
+  @Test
+  public void testDecodeString() {
+    // TODO: Implement test for string decoding
+    assertThat(true, equalTo(true));
+  }
+
+  /**
+   * Test decoding of list bencode format.
+   */
+  @Test
+  public void testDecodeList() {
+    // TODO: Implement test for list decoding
+    assertThat(true, equalTo(true));
+  }
+
+  /**
+   * Test decoding of dictionary bencode format.
+   */
+  @Test
+  public void testDecodeDictionary() {
+    // TODO: Implement test for dictionary decoding
+    assertThat(true, equalTo(true));
+  }
+}
